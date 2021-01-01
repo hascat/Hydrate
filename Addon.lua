f2c8eced8d321ce15709c7a47d06ce9fdc09d60f
Hascat <65823270+hascat@users.noreply.github.com>
2021-01-01 12:38:01 -0800

    Rework reminder scheduling and detection of allowed states.

--- Addon.lua
+++ Addon.lua
@@ -1,10 +1,15 @@
 local addonName, addon = ...
 
-local function ShowReminder()
-    if not addon.reminderActive then
-        return
-    end
+-- The reminder should only be shown when it's not inconvenient to the player. 
+local function AllowReminder()
+    local inCombat = addon.inCombat
+    local inPvP = C_PvP.IsArena() or C_PvP.IsBattleground()
+    local isBusy = UnitIsAFK("player") or UnitIsDND("player")
+    return not (inCombat or inPvP or isBusy)
+end
 
+-- Show the reminder frame if it is not already shown.
+local function ShowReminder()
     if addon.reminderFrame:IsShown() then
         return
     end
@@ -14,39 +19,39 @@ local function ShowReminder()
     addon.reminderFrame:Show()
 end
 
-local function ResetReminder()
-    if not addon.reminderActive then
+-- Determine if the reminder should be hidden or shown. The reminder will only
+-- be shown once the next schedule time is met, and the criteria are met to
+-- allow the reminder to be shown.
+local function UpdateReminder()
+    if not AllowReminder() then
+        addon.reminderFrame:Hide()
         return
     end
 
-    addon.reminderStartTime = GetServerTime()
-    C_Timer.After(addon.reminderInterval, ShowReminder)
+    -- If we haven't hit the timeout yet, do nothing.
+    local remainder = addon.nextReminderTime - GetServerTime()
+    if remainder > 0 then
+        C_Timer.After(remainder, UpdateReminder)
+        return
+    end
+
+    ShowReminder()
 end
 
-function addon:LoadReminder()
+-- Set a new time for showing the reminder.
+local function ResetReminder()
+    addon.nextReminderTime = GetServerTime() + addon.reminderInterval
+    UpdateReminder()
+end
+
+-- This is called for a variety of events.
+function addon:CheckReminder()
+    UpdateReminder()
+end
+
+-- Starts reminders being shown.
+function addon:StartReminder()
     self.reminderFrame.model:SetDisplayInfo(self.reminderDisplayId)
     self.reminderFrame:SetScript("OnHide", ResetReminder)
-
-    self:StartReminder()
-end
-
-function addon:StartReminder()
-    self.reminderActive = true
-
-    local remainder = 0
-    if self.reminderStartTime ~= nil then
-        local duration = GetServerTime() - self.reminderStartTime
-        remainder = self.reminderInterval - duration
-    end
-
-    if remainder < addon.minimumReminderDuration then
-        remainder = addon.minimumReminderDuration
-    end
-
-    C_Timer.After(remainder, ShowReminder)
-end
-
-function addon:StopReminder()
-    self.reminderActive = false
-    self.reminderFrame:Hide()
+    ResetReminder()
 end
--- Events.lua
+++ Events.lua
@@ -24,31 +24,36 @@ function events:ADDON_LOADED(_, name)
     self:RegisterEvent("PLAYER_REGEN_DISABLED")
     self:RegisterEvent("PLAYER_REGEN_ENABLED")
     self:RegisterEvent("PLAYER_FLAGS_CHANGED")
+    self:RegisterEvent("ZONE_CHANGED")
 end
 
 -- Start reminders once the player is in the game.
 function events:PLAYER_ENTERING_WORLD(...)
-    addon:LoadReminder()
+    addon:StartReminder()
 end
 
 -- This is fired when the player enters combat. Reminders should not be shown in
 -- combat to avoid distracting the player.
 function events:PLAYER_REGEN_DISABLED(...)
-    addon:StopReminder()
+    addon.inCombat = true
+    addon:CheckReminder()
 end
 
 -- This is fired when the player leaves combat. Remindes can be shown again.
 function events:PLAYER_REGEN_ENABLED(...)
-    addon:StartReminder()
+    addon.inCombat = false
+    addon:CheckReminder()
 end
 
 -- This is fired when the player AFK or DND flags change state. Reminders should
 -- not be shown while the player is AFK or DND as the player is unlikely to
 -- respond to them.
 function events:PLAYER_FLAGS_CHANGED(...)
-    if UnitIsAFK("player") or UnitIsDND("player") then
-        addon:StopReminder()
-    else
-        addon:StartReminder()
-    end
+    addon:CheckReminder()
+end
+
+-- This is fired when the player changes zones. Reminders should not be shown
+-- while the player is in an Arean or Battleground.
+function events:ZONE_CHANGED(...)
+    addon:CheckReminder()
 end
